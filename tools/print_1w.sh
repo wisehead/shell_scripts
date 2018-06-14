5e72f639275984b9334981faa657e34fef4e763a
wisehead <alex.chenhui@gmail.com>
2018-06-14 19:27:48 +0800

    while loop script.

---
+++ tools/print_1w.sh
@@ -0,0 +1,19 @@
+#############################################################
+#   File Name: alter_encrypt_key.sh
+#     Autohor: Hui Chen (c) 2017
+#        Mail: chenhui13@baidu.com
+# Create Time: 2017/06/28-19:56:03
+#############################################################
+#!/bin/sh 
+#!/bin/sh
+for i in {10000000001..10000010000}
+    do 
+		#a=1130102150229180616010000000000
+		a=113010215022918061601
+        #echo $i 
+		#c=$(expr $a + $i)
+		#echo $a+$i
+		echo $i
+        #head -n $var $1|tail -1 >>error.wrong.sql  
+        #sleep 10
+    done    
