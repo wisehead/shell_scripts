3164efc9de215837bb2aaf0b502742fbd0924af1
wisehead <alex.chenhui@gmail.com>
2020-03-22 16:20:42 +0800

    bool_calc/true_false

---
+++ bool_calc/true_false/true_false.sh
@@ -0,0 +1,11 @@
+#############################################################
+#   File Name: true_false.sh
+#     Autohor: Hui Chen (c) 2020
+#        Mail: chenhui13@baidu.com
+# Create Time: 2020/03/22-16:17:24
+#############################################################
+#!/bin/bash 
+if true; then echo "YES"; else echo "NO"; fi
+#YES
+if false; then echo "YES"; else echo "NO"; fi
+#NO
