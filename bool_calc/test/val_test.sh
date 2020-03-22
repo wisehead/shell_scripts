d37572fc2a81eb6c0e20b6e4db9edf20c64caf65
wisehead <alex.chenhui@gmail.com>
2020-03-22 16:42:51 +0800

    value test

---
+++ bool_calc/test/val_test.sh
@@ -0,0 +1,11 @@
+#############################################################
+#   File Name: val_test.sh
+#     Autohor: Hui Chen (c) 2020
+#        Mail: chenhui13@baidu.com
+# Create Time: 2020/03/22-16:42:20
+#############################################################
+#!/bin/sh 
+if test 5 -eq 5;then echo "YES"; else echo "NO"; fi
+#YES
+if test 5 -ne 5;then echo "YES"; else echo "NO"; fi
+#NO
