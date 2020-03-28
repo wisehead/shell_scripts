20f51c9e709f50bf552d5288eae23f38ee909585
wisehead <alex.chenhui@gmail.com>
2020-03-28 19:01:36 +0800

    cut

--- string/cut/cut.sh
+++ string/cut/cut.sh
@@ -8,3 +8,11 @@
 #差点略掉cut小工具，它用起来和awk类似，-d指定分割符，如同awk用-F指定分割符一样；-f指定“域”，如同awk的$数字。
 var="get the length of me"
 echo $var | cut -d" " -f 5
+
+#范例：选取指定列
+#选取/etc/passwd文件中的用户名和组ID两列
+
+cat /etc/passwd | cut -d":" -f1,4
+#选取/etc/group文件中的组名和组ID两列
+
+cat /etc/group | cut -d":" -f1,3
